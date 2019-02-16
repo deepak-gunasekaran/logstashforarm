ba1f2d9b65fbc2e088a120cbdc31fe12a1e0ae30
deepak-gunasekaran <dpk.com@gmail.com>
2019-02-16 10:17:32 -0600

    Update docker-entrypoint.sh
    Adding ENV

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,10 @@ fi
 
 # Run as user "logstash" if the command is "logstash"
 if [ "$1" = 'logstash' ]; then
+#if [ "$ELASTICSEARCH_URL" ]; then
+		sed -ri "s|ELASTICHOST|${ELASTICHOST}|g" /etc/logstash/logstash.conf
+		sed -ri "s|ELASTICPORT|${ELASTICPORT}|g" /etc/logstash/logstash.conf
+#	fi
 	set -- gosu logstash "$@"
 fi
 
